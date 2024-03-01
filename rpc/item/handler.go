437bc4a518947ea873d6b23f64da888accb140ce
29071 <lovestudy@qq.com>
2024-03-01 18:48:32 +0800

    Initial commit

---
+++ rpc/item/handler.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	item "example_shop/kitex_gen/example/shop/item"
+)
+
+// ItemServiceImpl implements the last service interface defined in the IDL.
+type ItemServiceImpl struct{}
+
+// GetItem implements the ItemServiceImpl interface.
+func (s *ItemServiceImpl) GetItem(ctx context.Context, req *item.GetItemReq) (resp *item.GetItemResp, err error) {
+	// TODO: Your code here...
+	return
+}
