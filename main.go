f68a836940a3ad09ba3f7e5b9d6e85e97dd4a42b
29071 <lovestudy@qq.com>
2024-03-02 11:15:22 +0800

    nacos

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"context"
-	"mykitex/kitex_gen/example/shop/item"
-
-	"github.com/cloudwego/hertz/pkg/app"
-	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/kitex/client"
-	"github.com/cloudwego/kitex/client/callopt"
+	"github.com/kitex-contrib/registry-nacos/v2/resolver"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 	"log"
 	"mykitex/kitex_gen/example/shop/item/itemservice"
-	"time"
 )
 
 var (
@@ -18,28 +15,33 @@ var (
 )
 
 func main() {
-	c, err := itemservice.NewClient("example.shop.item", client.WithHostPorts("0.0.0.0:8888"))
+	sc := []constant.ServerConfig{
+		*constant.NewServerConfig("127.0.0.1", 8848),
+	}
+	cc := constant.ClientConfig{
+		NamespaceId:         "public",
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+		LogLevel:            "info",
+		Username:            "your-name",
+		Password:            "your-password",
+	}
+
+	cli, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &cc,
+			ServerConfigs: sc,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	my_client, err := itemservice.NewClient("echo", client.WithResolver(resolver.NewNacosResolver(cli)))
 	if err != nil {
 		log.Fatal(err)
 	}
-	cli = c
+	log.Println(my_client)
 
-	hz := server.New(server.WithHostPorts("localhost:8889"))
-
-	hz.GET("/api/item", Handler)
-
-	if err := hz.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func Handler(ctx context.Context, c *app.RequestContext) {
-	req := &item.GetItemReq{Id: 1}
-	req.Id = 1024
-	resp, err := cli.GetItem(context.Background(), req, callopt.WithRPCTimeout(3*time.Second))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	c.String(200, resp.String())
 }
